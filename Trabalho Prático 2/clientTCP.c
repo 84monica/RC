44c725d1cfd5d55bbf9f0763ec0f8d24d00ddec9
84monica <up201905753@fe.up.pt>
2022-11-12 15:05:18 +0000

    Start Developing download application

--- Trabalho Prático 2/clientTCP.c
+++ Trabalho Prático 2/clientTCP.c
@@ -10,18 +10,60 @@
 #include <unistd.h>
 
 #include <string.h>
+#include <ctype.h>
 
-#define SERVER_PORT 6000
-#define SERVER_ADDR "192.168.28.96"
+#define FALSE 0
+#define TRUE 1
+#define SERVER_PORT 21
+#define SERVER_ADDR "193.137.29.15"
+
+void readResponse(int sockfd) {
+    /*read response*/
+    int digit_counter = 0; int end_read=FALSE;
+    while ( TRUE )
+    {
+        // read char
+        char ch;
+        size_t bytes = read(sockfd, &ch, 1);
+        if (bytes > 0) {
+            printf("%c", ch);
+        }
+        else {
+            perror("read()");
+            exit(-1);
+        }
+
+        // get code received
+        if (isdigit(ch)) {
+            digit_counter++;
+            continue;
+        }
+        
+        // end reading response condition
+        if (digit_counter == 3 && ch == ' ') {
+            end_read = TRUE;
+            digit_counter = 0;
+        }
+        if (end_read == TRUE && ch == '\n') {
+            break;
+        }
+        // not end reading response condition
+        if (digit_counter == 3 && ch != ' ') {
+            digit_counter = 0;
+        }
+    }
+}
 
 int main(int argc, char **argv) {
 
-    if (argc > 1)
-        printf("**** No arguments needed. They will be ignored. Carrying ON.\n");
+    // if (argc != 2) {
+    //     printf("Error Argmuments.\n");
+    //     return -1;
+    // }
+    /*get arguments*/
+
     int sockfd;
     struct sockaddr_in server_addr;
-    char buf[] = "Mensagem de teste na travessia da pilha TCP/IP\n";
-    size_t bytes;
 
     /*server address handling*/
     bzero((char *) &server_addr, sizeof(server_addr));
@@ -41,14 +83,39 @@ int main(int argc, char **argv) {
         perror("connect()");
         exit(-1);
     }
-    /*send a string to the server*/
-    bytes = write(sockfd, buf, strlen(buf));
+    readResponse(sockfd);
+
+    /*loggin in server*/
+    char user[] = "user anonymous\n";
+    size_t bytes = write(sockfd, user, strlen(user));
     if (bytes > 0)
         printf("Bytes escritos %ld\n", bytes);
     else {
         perror("write()");
         exit(-1);
     }
+    readResponse(sockfd);
+
+    char pass[] = "pass qualquer-password\n";
+    bytes = write(sockfd, pass, strlen(pass));
+    if (bytes > 0)
+        printf("Bytes escritos %ld\n", bytes);
+    else {
+        perror("write()");
+        exit(-1);
+    }
+    readResponse(sockfd);
+
+    /*passv command*/
+    char pasv[] = "pasv\n";
+    bytes = write(sockfd, pasv, strlen(pasv));
+    if (bytes > 0)
+        printf("Bytes escritos %ld\n", bytes);
+    else {
+        perror("write()");
+        exit(-1);
+    }
+    readResponse(sockfd);
 
     if (close(sockfd)<0) {
         perror("close()");
